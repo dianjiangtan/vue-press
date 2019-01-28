c067c1c22be592a99c9aae803d37dfc76b205c7b
tanchuan <tanchuan@zbj.com>
2019-01-28 11:18:07 +0800

    自动部署

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://${ACCESS_TOKEN}@github.com:dianjiangtan/dianjiangtan.github.io.git master
+git push -f https://${ACCESS_TOKEN}@github.com/dianjiangtan/dianjiangtan.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:diangjiangtan/vue-press.git dev:dev
