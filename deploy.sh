77910d603b92c0e38db69d4c4cbbb3ea878b2f6d
tanchuan <tanchuan@zbj.com>
2019-01-31 17:50:30 +0800

    自动部署

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push 'https://${GH_TOKEN}@github.com/dianjiangtan/dianjiangtan.github.io.git' master
+git push https://github.com/dianjiangtan/dianjiangtan.github.io.git master
 
 cd -
